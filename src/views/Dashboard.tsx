0b73f57890a62168f8f6378713c47dfc36cc1ccc
Afaan Bilal <afaanbilal@gmail.com>
2023-03-01 17:39:25 +0530

    ✨ Dashboard: redesign

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -6,12 +6,12 @@
  * @link   https://github.com/AfaanBilal/iron-guard-web
  */
 
-import type { Component } from "solid-js";
+import { Component, Show } from "solid-js";
 
 const Dashboard: Component = () => {
     return (
         <div class="flex-grow flex flex-col">
-            <div class="flex flex-wrap gap-4 p-4 border-b border-slate-500">
+            <div class="flex flex-wrap gap-4 p-4">
                 <div class="flex-grow h-48 pb-4 grid grid-cols-2 items-end border bg-slate-700 border-slate-600 rounded-lg">
                     <div class="ml-8">
                         <img class="w-32 h-32" src={"/src/assets/icon.png"} alt="icon" />
@@ -27,12 +27,14 @@ const Dashboard: Component = () => {
                 <div class="w-64 h-48 grid items-center text-center border bg-slate-700 border-slate-600 rounded-lg">
                     <div class="text-6xl">512 <br /><span class="text-lg uppercase">categories</span></div>
                 </div>
-                <div class="w-64 h-48 grid items-center text-center border bg-slate-700 border-slate-600 rounded-lg">
-                    <div class="text-6xl">10 <br /><span class="text-lg uppercase">users</span></div>
-                </div>
+                <Show when={0 === 0}>
+                    <div class="w-64 h-48 grid items-center text-center border bg-slate-700 border-slate-600 rounded-lg">
+                        <div class="text-6xl">10 <br /><span class="text-lg uppercase">users</span></div>
+                    </div>
+                </Show>
             </div>
-            <div class="flex flex-wrap">
-                <div class="overflow-x-auto px-2">
+            <div class="flex gap-4">
+                <div class="overflow-x-auto px-4 flex-1">
                     <h1 class="text-3xl p-4">Latest Items</h1>
                     <table class="w-full whitespace-nowrap rounded-lg divide-y divide-gray-300 overflow-hidden">
                         <thead class="bg-gray-900">
@@ -71,41 +73,35 @@ const Dashboard: Component = () => {
                         </tbody>
                     </table>
                 </div>
-                <div class="overflow-x-auto px-2">
+                <div class="overflow-x-auto px-4 flex-1">
                     <h1 class="text-3xl p-4">Latest Categories</h1>
                     <table class="w-full whitespace-nowrap rounded-lg divide-y divide-gray-300 overflow-hidden">
                         <thead class="bg-gray-900">
                             <tr class="text-white text-left">
                                 <th class="font-semibold text-sm uppercase px-6 py-4">Category</th>
                                 <th class="font-semibold text-sm uppercase px-6 py-4">Description</th>
-                                <th class="font-semibold text-sm uppercase px-6 py-4">Item Count</th>
                             </tr>
                         </thead>
                         <tbody class="divide-y divide-gray-600 bg-slate-800">
                             <tr>
                                 <td class="px-6 py-4">Electronics</td>
                                 <td class="px-6 py-4">Electronics</td>
-                                <td class="px-6 py-4">42</td>
                             </tr>
                             <tr>
                                 <td class="px-6 py-4">Keyboard</td>
                                 <td class="px-6 py-4">Electronics</td>
-                                <td class="px-6 py-4">8</td>
                             </tr>
                             <tr>
                                 <td class="px-6 py-4">Mouse</td>
                                 <td class="px-6 py-4">Electronics</td>
-                                <td class="px-6 py-4">8</td>
                             </tr>
                             <tr>
                                 <td class="px-6 py-4">Mouse</td>
                                 <td class="px-6 py-4">Electronics</td>
-                                <td class="px-6 py-4">8</td>
                             </tr>
                             <tr>
                                 <td class="px-6 py-4">Mouse</td>
                                 <td class="px-6 py-4">Electronics</td>
-                                <td class="px-6 py-4">8</td>
                             </tr>
                         </tbody>
                     </table>
