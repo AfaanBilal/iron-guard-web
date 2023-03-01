00f7a1e8503cbd0735537c9921c403acee3c4eb0
Afaan Bilal <afaanbilal@gmail.com>
2023-03-01 12:36:28 +0530

    ✨ View: sign in

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@
  * @link   https://github.com/AfaanBilal/iron-guard-web
  */
 
-import { type Component, lazy } from "solid-js";
+import { type Component, lazy, createSignal, Show } from "solid-js";
 import { Routes, Route, A, useLocation } from "@solidjs/router";
+import SignIn from "./views/Auth/SignIn";
 
 const Dashboard = lazy(() => import("./views/Dashboard"));
 const ListItems = lazy(() => import("./views/Items/ListItems"));
@@ -15,29 +16,33 @@ const ListCategories = lazy(() => import("./views/Categories/ListCategories"));
 const Users = lazy(() => import("./views/Users/ListUsers"));
 
 const App: Component = () => {
+    const [token, setToken] = createSignal("");
     const location = useLocation();
 
     return (
         <div class="h-screen flex-grow flex flex-col text-white bg-slate-900">
-            <div class="bg-gray-700 p-4">
-                <h1 class="text-3xl font-bold">Iron Guard</h1>
-            </div>
-            <div class="flex-grow flex">
-                <div class="w-48 bg-gray-800 border-r border-r-slate-600 text-lg text-gray-300">
-                    <A href="/"><div class={`px-4 py-4 border-b border-b-slate-600 ${location.pathname == "/" ? "bg-black" : ""}`}>Dashboard</div></A>
-                    <A href="/items"><div class={`px-4 py-4 border-b border-b-slate-600 ${location.pathname.includes("items") ? "bg-black" : ""}`}>Items</div></A>
-                    <A href="/categories"><div class={`px-4 py-4 border-b border-b-slate-600 ${location.pathname.includes("categories") ? "bg-black" : ""}`}>Catagories</div></A>
-                    <A href="/users"><div class={`px-4 py-4 border-b border-b-slate-600 ${location.pathname.includes("users") ? "bg-black" : ""}`}>Users</div></A>
+            <Show when={token() === ""}><SignIn /></Show>
+            <Show when={token() !== ""}>
+                <div class="bg-gray-700 p-4">
+                    <h1 class="text-3xl font-bold">Iron Guard</h1>
                 </div>
-                <div class="flex-grow">
-                    <Routes>
-                        <Route path="/" component={Dashboard} />
-                        <Route path="/items" component={ListItems} />
-                        <Route path="/categories" component={ListCategories} />
-                        <Route path="/users" component={Users} />
-                    </Routes>
+                <div class="flex-grow flex">
+                    <div class="w-48 bg-gray-800 border-r border-r-slate-600 text-lg text-gray-300">
+                        <A href="/"><div class={`px-4 py-4 border-b border-b-slate-600 ${location.pathname == "/" ? "bg-black" : ""}`}>Dashboard</div></A>
+                        <A href="/items"><div class={`px-4 py-4 border-b border-b-slate-600 ${location.pathname.includes("items") ? "bg-black" : ""}`}>Items</div></A>
+                        <A href="/categories"><div class={`px-4 py-4 border-b border-b-slate-600 ${location.pathname.includes("categories") ? "bg-black" : ""}`}>Catagories</div></A>
+                        <A href="/users"><div class={`px-4 py-4 border-b border-b-slate-600 ${location.pathname.includes("users") ? "bg-black" : ""}`}>Users</div></A>
+                    </div>
+                    <div class="flex-grow">
+                        <Routes>
+                            <Route path="/" component={Dashboard} />
+                            <Route path="/items" component={ListItems} />
+                            <Route path="/categories" component={ListCategories} />
+                            <Route path="/users" component={Users} />
+                        </Routes>
+                    </div>
                 </div>
-            </div>
+            </Show>
         </div>
     );
 };
---
+++ src/views/Auth/SignIn.tsx
@@ -0,0 +1,39 @@
+/**
+ * Iron Guard Web
+ *
+ * @author Afaan Bilal
+ * @link   https://afaan.dev
+ * @link   https://github.com/AfaanBilal/iron-guard-web
+ */
+
+import type { Component } from "solid-js";
+
+const SignIn: Component = () => {
+    return (
+        <div class="flex-grow grid place-items-center">
+            <div class="bg-slate-700 p-8 rounded-2xl">
+                <div class="grid place-items-center pb-4">
+                    <img class="w-48 h-48" src={"/src/assets/icon.png"} />
+                </div>
+                <div class="p-4">
+                    <h1 class="pb-4 border-b border-b-slate-500 text-6xl font-extrabold text-center">Iron Guard</h1>
+                    <p class="mt-4 text-2xl text-center">Sign In</p>
+                </div>
+                <div class="p-4">
+                    <input type="email" placeholder="Email" class="w-full px-4 py-2 rounded text-xl" />
+                </div>
+                <div class="p-4">
+                    <input type="password" placeholder="Password" class="w-full px-4 py-2 rounded text-xl" />
+                </div>
+                <div class="p-4 text-center">
+                    <button type="button" class="w-full p-2 bg-slate-600 rounded-lg text-xl">Sign In</button>
+                </div>
+                <div class="p-4 pt-8 text-center">
+                    &copy; <a class="text-zinc-400" href="https://afaan.dev" target="_blank">Afaan Bilal</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SignIn;
