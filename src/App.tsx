4117ed1ed72e3475854cbb0f43b0c0803645960c
Afaan Bilal <afaanbilal@gmail.com>
2023-03-01 16:23:12 +0530

    ✨ Nav

--- src/App.tsx
+++ src/App.tsx
@@ -27,17 +27,22 @@ const App: Component = () => {
         <div class="h-screen flex-grow flex flex-col text-white bg-slate-900">
             <Show when={token() === ""}><SignIn /></Show>
             <Show when={token() !== ""}>
-                <div class="bg-gray-700 p-4 flex">
-                    <img class="w-8 h-8" src={"/src/assets/icon.png"} alt="icon" />
-                    <h1 class="ml-4 text-3xl font-bold">Iron Guard</h1>
-                </div>
+                <A href="/">
+                    <div class="bg-gray-700 p-4 flex">
+                        <img class="w-8 h-8" src={"/src/assets/icon.png"} alt="icon" />
+                        <h1 class="ml-4 text-3xl font-bold">Iron Guard</h1>
+                    </div>
+                </A>
                 <div class="flex-grow flex">
-                    <div class="w-64 bg-gray-800 border-r border-r-slate-600 text-lg text-gray-300">
+                    <div class="w-64 flex flex-col bg-gray-800 border-r border-r-slate-600 text-lg text-gray-300">
                         <A href="/"><div class={`px-4 py-4 border-b border-b-slate-600 ${location.pathname == "/" ? "bg-black" : ""}`}>Dashboard</div></A>
                         <A href="/inventory"><div class={`px-4 py-4 border-b border-b-slate-600 ${location.pathname.includes("inventory") ? "bg-black" : ""}`}>Inventory</div></A>
                         <A href="/items"><div class={`px-4 py-4 border-b border-b-slate-600 ${location.pathname.includes("items") ? "bg-black" : ""}`}>Items</div></A>
                         <A href="/categories"><div class={`px-4 py-4 border-b border-b-slate-600 ${location.pathname.includes("categories") ? "bg-black" : ""}`}>Categories</div></A>
                         <A href="/users"><div class={`px-4 py-4 border-b border-b-slate-600 ${location.pathname.includes("users") ? "bg-black" : ""}`}>Users</div></A>
+                        <div class="absolute bottom-0 text-center pl-4 pb-4">
+                            &copy; <a class="text-zinc-400" href="https://afaan.dev" target="_blank">Afaan Bilal</a>
+                        </div>
                     </div>
                     <div class="flex-grow">
                         <Routes>
