f6937558fae7c020f354de1bf5c4dbe6ca50fbdc
Afaan Bilal <afaanbilal@gmail.com>
2023-03-01 11:47:07 +0530

    🚧 Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,33 @@
  * @link   https://github.com/AfaanBilal/iron-guard-web
  */
 
-import type { Component } from "solid-js";
+import { type Component, lazy } from "solid-js";
+import { Routes, Route, A } from "@solidjs/router";
+
+const Dashboard = lazy(() => import("./views/Dashboard"));
 
 const App: Component = () => {
     return (
-        <div>
-            Iron Guard
+        <div class="h-screen flex-grow flex flex-col text-white bg-slate-900">
+            <div class="bg-gray-700 p-4">
+                <h1 class="text-3xl font-bold">Iron Guard</h1>
+            </div>
+            <div class="flex-grow flex">
+                <div class="w-48 bg-gray-800 border-r border-r-slate-600 text-lg text-gray-300">
+                    <A href="/"><div class="px-4 py-4 border-b border-b-slate-600">Dashboard</div></A>
+                    <A href="/items"><div class="px-4 py-4 border-b border-b-slate-600">Items</div></A>
+                    <A href="/categories"><div class="px-4 py-4 border-b border-b-slate-600">Catagories</div></A>
+                    <A href="/users"><div class="px-4 py-4 border-b border-b-slate-600">Users</div></A>
+                </div>
+                <div class="flex-grow">
+                    <Routes>
+                        <Route path="/" component={Dashboard} />
+                        <Route path="/items" element={<div>This is items</div>} />
+                        <Route path="/categories" element={<div>This site was made with Solid</div>} />
+                        <Route path="/users" element={<div>This site was made with Solid</div>} />
+                    </Routes>
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/views/Dashboard.tsx
@@ -0,0 +1,113 @@
+/**
+ * Iron Guard Web
+ *
+ * @author Afaan Bilal
+ * @link   https://afaan.dev
+ * @link   https://github.com/AfaanBilal/iron-guard-web
+ */
+
+import type { Component } from "solid-js";
+
+const Dashboard: Component = () => {
+    return (
+        <div class="flex-grow flex flex-col">
+            <div class="flex gap-4 p-4 border-b border-slate-500">
+                <div class="flex-grow h-48 pb-4 grid grid-cols-2 items-end border bg-slate-700 border-slate-600 rounded-lg">
+                    <div class="ml-8 text-9xl">Iron Guard</div>
+                    <div class="mr-4 text-4xl text-gray-500 uppercase text-right">inventory management system</div>
+                </div>
+                <div class="w-64 h-48 grid items-center text-center border bg-slate-700 border-slate-600 rounded-lg">
+                    <div class="text-6xl">1234 <br /><span class="text-lg uppercase">items</span></div>
+                </div>
+                <div class="w-64 h-48 grid items-center text-center border bg-slate-700 border-slate-600 rounded-lg">
+                    <div class="text-6xl">512 <br /><span class="text-lg uppercase">categories</span></div>
+                </div>
+                <div class="w-64 h-48 grid items-center text-center border bg-slate-700 border-slate-600 rounded-lg">
+                    <div class="text-6xl">10 <br /><span class="text-lg uppercase">users</span></div>
+                </div>
+            </div>
+            <div class="flex">
+                <div class="overflow-x-auto w-full px-2">
+                    <h1 class="text-3xl p-4">Latest Items</h1>
+                    <table class="w-full whitespace-nowrap rounded-lg divide-y divide-gray-300 overflow-hidden">
+                        <thead class="bg-gray-900">
+                            <tr class="text-white text-left">
+                                <th class="font-semibold text-sm uppercase px-6 py-4">Item</th>
+                                <th class="font-semibold text-sm uppercase px-6 py-4">Description</th>
+                                <th class="font-semibold text-sm uppercase px-6 py-4">Quantity</th>
+                            </tr>
+                        </thead>
+                        <tbody class="divide-y divide-gray-600 bg-slate-800">
+                            <tr>
+                                <td class="px-6 py-4">Monitor</td>
+                                <td class="px-6 py-4">Monitor</td>
+                                <td class="px-6 py-4">42</td>
+                            </tr>
+                            <tr>
+                                <td class="px-6 py-4">Keyboard</td>
+                                <td class="px-6 py-4">Monitor</td>
+                                <td class="px-6 py-4">8</td>
+                            </tr>
+                            <tr>
+                                <td class="px-6 py-4">Mouse</td>
+                                <td class="px-6 py-4">Monitor</td>
+                                <td class="px-6 py-4">8</td>
+                            </tr>
+                            <tr>
+                                <td class="px-6 py-4">Mouse</td>
+                                <td class="px-6 py-4">Monitor</td>
+                                <td class="px-6 py-4">8</td>
+                            </tr>
+                            <tr>
+                                <td class="px-6 py-4">Mouse</td>
+                                <td class="px-6 py-4">Monitor</td>
+                                <td class="px-6 py-4">8</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="overflow-x-auto w-full px-2">
+                    <h1 class="text-3xl p-4">Latest Categories</h1>
+                    <table class="w-full whitespace-nowrap rounded-lg divide-y divide-gray-300 overflow-hidden">
+                        <thead class="bg-gray-900">
+                            <tr class="text-white text-left">
+                                <th class="font-semibold text-sm uppercase px-6 py-4">Category</th>
+                                <th class="font-semibold text-sm uppercase px-6 py-4">Description</th>
+                                <th class="font-semibold text-sm uppercase px-6 py-4">Item Count</th>
+                            </tr>
+                        </thead>
+                        <tbody class="divide-y divide-gray-600 bg-slate-800">
+                            <tr>
+                                <td class="px-6 py-4">Electronics</td>
+                                <td class="px-6 py-4">Electronics</td>
+                                <td class="px-6 py-4">42</td>
+                            </tr>
+                            <tr>
+                                <td class="px-6 py-4">Keyboard</td>
+                                <td class="px-6 py-4">Electronics</td>
+                                <td class="px-6 py-4">8</td>
+                            </tr>
+                            <tr>
+                                <td class="px-6 py-4">Mouse</td>
+                                <td class="px-6 py-4">Electronics</td>
+                                <td class="px-6 py-4">8</td>
+                            </tr>
+                            <tr>
+                                <td class="px-6 py-4">Mouse</td>
+                                <td class="px-6 py-4">Electronics</td>
+                                <td class="px-6 py-4">8</td>
+                            </tr>
+                            <tr>
+                                <td class="px-6 py-4">Mouse</td>
+                                <td class="px-6 py-4">Electronics</td>
+                                <td class="px-6 py-4">8</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Dashboard;
