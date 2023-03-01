e9f5e7a79bb66c59c8f762ca84e62ef48de62432
Afaan Bilal <afaanbilal@gmail.com>
2023-03-01 12:02:55 +0530

    ✨ View: items

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,14 @@
  */
 
 import { type Component, lazy } from "solid-js";
-import { Routes, Route, A } from "@solidjs/router";
+import { Routes, Route, A, useLocation } from "@solidjs/router";
 
 const Dashboard = lazy(() => import("./views/Dashboard"));
+const ListItems = lazy(() => import("./views/Items/ListItems"));
 
 const App: Component = () => {
+    const location = useLocation();
+
     return (
         <div class="h-screen flex-grow flex flex-col text-white bg-slate-900">
             <div class="bg-gray-700 p-4">
@@ -19,17 +22,17 @@ const App: Component = () => {
             </div>
             <div class="flex-grow flex">
                 <div class="w-48 bg-gray-800 border-r border-r-slate-600 text-lg text-gray-300">
-                    <A href="/"><div class="px-4 py-4 border-b border-b-slate-600">Dashboard</div></A>
-                    <A href="/items"><div class="px-4 py-4 border-b border-b-slate-600">Items</div></A>
-                    <A href="/categories"><div class="px-4 py-4 border-b border-b-slate-600">Catagories</div></A>
-                    <A href="/users"><div class="px-4 py-4 border-b border-b-slate-600">Users</div></A>
+                    <A href="/"><div class={`px-4 py-4 border-b border-b-slate-600 ${location.pathname == "/" ? "bg-black" : ""}`}>Dashboard</div></A>
+                    <A href="/items"><div class={`px-4 py-4 border-b border-b-slate-600 ${location.pathname.includes("items") ? "bg-black" : ""}`}>Items</div></A>
+                    <A href="/categories"><div class={`px-4 py-4 border-b border-b-slate-600 ${location.pathname.includes("categories") ? "bg-black" : ""}`}>Catagories</div></A>
+                    <A href="/users"><div class={`px-4 py-4 border-b border-b-slate-600 ${location.pathname.includes("users") ? "bg-black" : ""}`}>Users</div></A>
                 </div>
                 <div class="flex-grow">
                     <Routes>
                         <Route path="/" component={Dashboard} />
-                        <Route path="/items" element={<div>This is items</div>} />
-                        <Route path="/categories" element={<div>This site was made with Solid</div>} />
-                        <Route path="/users" element={<div>This site was made with Solid</div>} />
+                        <Route path="/items" component={ListItems} />
+                        <Route path="/categories" element={<div>Categories</div>} />
+                        <Route path="/users" element={<div>Users</div>} />
                     </Routes>
                 </div>
             </div>
---
+++ src/views/Items/ListItems.tsx
@@ -0,0 +1,60 @@
+/**
+ * Iron Guard Web
+ *
+ * @author Afaan Bilal
+ * @link   https://afaan.dev
+ * @link   https://github.com/AfaanBilal/iron-guard-web
+ */
+
+import { type Component, For } from "solid-js";
+
+const items = [
+    { name: "Monitor", description: "A monitor is a window into the workings of electricity.", quantity: 42 },
+    { name: "Monitor", description: "A monitor is a window into the workings of electricity.", quantity: 42 },
+    { name: "Monitor", description: "A monitor is a window into the workings of electricity.", quantity: 42 },
+    { name: "Monitor", description: "A monitor is a window into the workings of electricity.", quantity: 42 },
+    { name: "Monitor", description: "A monitor is a window into the workings of electricity.", quantity: 42 },
+    { name: "Monitor", description: "A monitor is a window into the workings of electricity.", quantity: 42 },
+    { name: "Monitor", description: "A monitor is a window into the workings of electricity.", quantity: 42 },
+    { name: "Monitor", description: "A monitor is a window into the workings of electricity.", quantity: 42 },
+    { name: "Monitor", description: "A monitor is a window into the workings of electricity.", quantity: 42 },
+    { name: "Monitor", description: "A monitor is a window into the workings of electricity.", quantity: 42 },
+    { name: "Monitor", description: "A monitor is a window into the workings of electricity.", quantity: 42 },
+    { name: "Monitor", description: "A monitor is a window into the workings of electricity.", quantity: 42 },
+    { name: "Monitor", description: "A monitor is a window into the workings of electricity.", quantity: 42 },
+    { name: "Monitor", description: "A monitor is a window into the workings of electricity.", quantity: 42 },
+];
+
+const ListItems: Component = () => {
+    return (
+        <div class="flex-grow flex flex-col px-2">
+            <h1 class="text-5xl p-4 mb-4">Items</h1>
+            <div class="overflow-x-auto w-full">
+                <table class="w-full whitespace-nowrap rounded-lg divide-y divide-gray-300 overflow-hidden">
+                    <thead class="bg-gray-900">
+                        <tr class="text-white text-left">
+                            <th class="font-semibold text-sm uppercase px-6 py-4">Item</th>
+                            <th class="font-semibold text-sm uppercase px-6 py-4">Description</th>
+                            <th class="font-semibold text-sm uppercase px-6 py-4">Quantity</th>
+                            <th class="font-semibold text-sm uppercase px-6 py-4">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody class="divide-y divide-gray-600 bg-slate-800">
+                        <For each={items}>
+                            {i =>
+                                <tr>
+                                    <td class="px-6 py-4">{i.name}</td>
+                                    <td class="px-6 py-4">{i.description}</td>
+                                    <td class="px-6 py-4">{i.quantity}</td>
+                                    <td class="px-6 py-4">View / Edit / Delete</td>
+                                </tr>
+                            }
+                        </For>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default ListItems;
