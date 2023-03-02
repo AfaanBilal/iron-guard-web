7d05612ad5368498eec8fb92709c7561c927ecf6
Afaan Bilal <afaanbilal@gmail.com>
2023-03-02 12:09:39 +0530

    ✨ User Editor

--- src/views/Users/UserEditor.tsx
+++ src/views/Users/UserEditor.tsx
@@ -6,13 +6,17 @@
  * @link   https://github.com/AfaanBilal/iron-guard-web
  */
 
-import { createSignal, Show, type Component } from "solid-js";
+import { useParams } from "@solidjs/router";
+import { createEffect, createResource, createSignal, Show, type Component } from "solid-js";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
 import Select from "../../components/Select";
-import { addUser, Status } from "../../definitions/api";
+import { addUser, getUser, Status, updateUser } from "../../definitions/api";
+import User from "../../definitions/types/User";
 
 const AddUser: Component = () => {
+    const params = useParams();
+
     const [role, setRole] = createSignal("user");
     const [firstname, setFirstname] = createSignal("");
     const [lastname, setLastname] = createSignal("");
@@ -20,13 +24,30 @@ const AddUser: Component = () => {
     const [password, setPassword] = createSignal("");
     const [error, setError] = createSignal("");
 
+    const [uuid, _] = createSignal(params.uuid || "");
+    const [data] = createResource(() => params.uuid || null, getUser);
+
+    createEffect(() => {
+        if (data().uuid) {
+            const u = data() as User;
+            setFirstname(u.firstname);
+            setLastname(u.lastname);
+            setEmail(u.email);
+        }
+    });
+
     const save = async () => {
-        if (firstname().trim() === "") {
+        if (firstname().trim() === "" || lastname().trim() === "" || email().trim() === "") {
             setError("Please fill firstname, lastname and email.");
             return;
         }
 
-        const r = await addUser(role(), firstname(), lastname(), email(), password());
+        let r = null;
+        if (uuid() === "") {
+            r = await addUser(role(), firstname(), lastname(), email(), password());
+        } else {
+            r = await updateUser(uuid(), role(), firstname(), lastname(), email(), password());
+        }
 
         if (r.status === Status.Success) {
             window.history.back();
@@ -37,45 +58,47 @@ const AddUser: Component = () => {
 
     return (
         <div class="flex-grow flex flex-col px-2">
-            <h1 class="px-4 py-2 my-4 text-3xl border-b border-b-slate-700">
-                Add User
-                <Show when={error() !== ""}>
-                    <span class="ml-4 px-4 py-2 rounded text-xl bg-red-900 text-gray-300">{error()}</span>
-                </Show>
-            </h1>
-            <div class="flex-grow p-4 bg-gray-700">
-                <div class="flex items-center py-2">
-                    <div class="px-2 text-slate-300 text-xl w-64">Firstname</div>
-                    <div class="px-2"><Input value={firstname()} onInput={e => setFirstname(e.currentTarget.value)} /></div>
-                </div>
-                <div class="flex items-center py-2">
-                    <div class="px-2 text-slate-300 text-xl w-64">Lastname</div>
-                    <div class="px-2"><Input value={lastname()} onInput={e => setLastname(e.currentTarget.value)} /></div>
-                </div>
-                <div class="flex items-center py-2">
-                    <div class="px-2 text-slate-300 text-xl w-64">Email</div>
-                    <div class="px-2"><Input type="email" value={email()} onInput={e => setEmail(e.currentTarget.value)} /></div>
-                </div>
-                <div class="flex items-center py-2">
-                    <div class="px-2 text-slate-300 text-xl w-64">Password</div>
-                    <div class="px-2"><Input type="password" value={password()} onInput={e => setPassword(e.currentTarget.value)} /></div>
-                </div>
-                <div class="flex items-center py-2">
-                    <div class="px-2 text-slate-300 text-xl w-64">Role</div>
-                    <div class="px-2">
-                        <Select
-                            label="Select a role"
-                            options={[{ value: "user", label: "User" }, { value: "admin", label: "Admin" }]}
-                            selected={role()}
-                            onChange={e => setRole(e.currentTarget.value)}
-                        />
+            <Show when={uuid() === "" || !data.loading}>
+                <h1 class="px-4 py-2 my-4 text-3xl border-b border-b-slate-700">
+                    {uuid() === "" ? "Add" : "Edit"} User
+                    <Show when={error() !== ""}>
+                        <span class="ml-4 px-4 py-2 rounded text-xl bg-red-900 text-gray-300">{error()}</span>
+                    </Show>
+                </h1>
+                <div class="flex-grow p-4 bg-gray-700">
+                    <div class="flex items-center py-2">
+                        <div class="px-2 text-slate-300 text-xl w-64">Firstname</div>
+                        <div class="px-2"><Input value={firstname()} onInput={e => setFirstname(e.currentTarget.value)} /></div>
+                    </div>
+                    <div class="flex items-center py-2">
+                        <div class="px-2 text-slate-300 text-xl w-64">Lastname</div>
+                        <div class="px-2"><Input value={lastname()} onInput={e => setLastname(e.currentTarget.value)} /></div>
+                    </div>
+                    <div class="flex items-center py-2">
+                        <div class="px-2 text-slate-300 text-xl w-64">Email</div>
+                        <div class="px-2"><Input type="email" value={email()} onInput={e => setEmail(e.currentTarget.value)} /></div>
+                    </div>
+                    <div class="flex items-center py-2">
+                        <div class="px-2 text-slate-300 text-xl w-64">Password</div>
+                        <div class="px-2"><Input type="password" value={password()} onInput={e => setPassword(e.currentTarget.value)} /></div>
+                    </div>
+                    <div class="flex items-center py-2">
+                        <div class="px-2 text-slate-300 text-xl w-64">Role</div>
+                        <div class="px-2">
+                            <Select
+                                label="Select a role"
+                                options={[{ value: "user", label: "User" }, { value: "admin", label: "Admin" }]}
+                                selected={role()}
+                                onChange={e => setRole(e.currentTarget.value)}
+                            />
+                        </div>
+                    </div>
+                    <div class="flex items-center px-2 py-4 mt-4 border-t border-t-slate-800 gap-2">
+                        <Button label="Save" onClick={save} />
+                        <Button label="Cancel" onClick={() => window.history.back()} />
                     </div>
                 </div>
-                <div class="flex items-center px-2 py-4 mt-4 border-t border-t-slate-800 gap-2">
-                    <Button label="Save" onClick={save} />
-                    <Button label="Cancel" onClick={() => window.history.back()} />
-                </div>
-            </div>
+            </Show>
         </div>
     );
 };
